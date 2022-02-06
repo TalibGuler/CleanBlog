b4738465fafa32410940c693ba74b4f81d60f1d4
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-02-06 19:15:33 +0300

    mongoose eklendi ve crud işlemleri yapıldı

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
-const express = require('express')
-const ejs = require('ejs')
-const app = express()
+const express = require('express');
+const moongose = require('mongoose');
+const ejs = require('ejs');
+const Post = require('./models/Post');
 
-app.set("view engine", "ejs")
+const app = express();
 
-app.use(express.static('public'))
+// CONNECT DB
+moongose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get('/', (req,res) =>{
-    // const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    // res.send(blog)
-    res.render('index')
-})
+app.set('view engine', 'ejs');
 
-app.get('/about',(req,res)=>{
-    res.render('about')
-})
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/add_post',(req,res)=>{
-    res.render('add_post')
-})
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  // const blog = { id: 1, title: "Blog title", description: "Blog description" }
+  // res.send(blog)
+  res.render('index', {
+    posts: posts
+  });
+});
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.get('/post',(req,res)=>{
-    res.render('post')
-})
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 
-const port = 3000
+// app.get('/post', (req, res) => {
+//   res.render('post');
+// });
 
-app.listen(port,()=>{
-    console.log(`Sunucu ${port} portunda başlatıldı...`)
-})
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
